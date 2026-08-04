369deddac83f1911a22258b556e63020b8571a4d
rmk062026 <rmk062026@gmail.com>
2026-08-04 14:02:13 +0200

    Added if string i empty. menu shows with 2 options

--- Program.cs
+++ Program.cs
@@ -10,4 +10,36 @@ Console.WriteLine("*********************");
 Console.WriteLine("Skriv ditt navn:");
 string userName = Console.ReadLine() ?? "";
 
-functions.GreetingText(userName);
+if (string.IsNullOrWhiteSpace(userName))
+{
+    while (string.IsNullOrWhiteSpace(userName))
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Navnet kan ikke være tomt.");
+        Console.WriteLine("1. Skriv navnet på nytt.");
+        Console.WriteLine("2. Avslutt programmet.");
+        Console.WriteLine("Velg mellom 1-2:");
+
+        string menuChoice = Console.ReadLine() ?? "";
+
+        if (menuChoice == "1")
+        {
+            Console.WriteLine("Skriv navnet ditt:");
+            userName = Console.ReadLine() ?? "";
+        }
+        else if (menuChoice == "2")
+        {
+            Console.WriteLine("Avslutter programmet...");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Ugyldig valg... Prøv igjen...");
+        }
+        functions.GreetingText(userName);
+    }
+}
+else
+{
+    functions.GreetingText(userName);
+}
