d0e83d27882a40c09462c7a882ac0451bd85d92c
rmk062026 <rmk062026@gmail.com>
2026-08-04 13:41:29 +0200

    commented out userNameEmpty bool (Not used yet)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Uke2Innlevering;
 // bool programRunning = true;
-bool userNameEmpty = true;
+// bool userNameEmpty = true;
 Functions functions = new Functions();
 functions.ClockOfDay();
 Console.WriteLine("Min innleverings oppgave\n");
