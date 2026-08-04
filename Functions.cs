3a6a481ad9ced4a14c36edd7e2ed937c7d5e41de
rmk062026 <rmk062026@gmail.com>
2026-08-04 13:37:41 +0200

    Added Hour check and greeting in Functions.cs

--- Functions.cs
+++ Functions.cs
@@ -6,6 +6,25 @@ public class Functions
     {
         DateTime clockNow = DateTime.Now;
 
-        Console.WriteLine(clockNow);
+        Console.WriteLine($"Klokken er: {clockNow:HH:mm}");
+    }
+
+    public void GreetingText(string userName)
+    {
+        int hour = DateTime.Now.Hour;
+        string greeting = "";
+
+        if (hour >= 6 && hour < 10)
+            greeting = "God morgen";
+        else if (hour >= 10 && hour < 14)
+            greeting = "God formiddag";
+        else if (hour >= 14 && hour < 18)
+            greeting = "God ettermiddag";
+        else if (hour >= 18 && hour < 23)
+            greeting = "God kveld";
+        else
+            greeting = "God natt";
+
+        Console.WriteLine($"{greeting}, {userName}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿using Uke2Innlevering;
-
+// bool programRunning = true;
+bool userNameEmpty = true;
 Functions functions = new Functions();
 functions.ClockOfDay();
+Console.WriteLine("Min innleverings oppgave\n");
+
+
+Console.WriteLine("*********************");
+Console.WriteLine("Skriv ditt navn:");
+string userName = Console.ReadLine() ?? "";
+
+functions.GreetingText(userName);
